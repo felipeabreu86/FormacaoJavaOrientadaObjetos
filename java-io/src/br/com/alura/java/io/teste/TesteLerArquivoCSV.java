035da8e93d37ffaf37450a4f63dd54e559685529
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-05-03 08:12:29 -0300

    Usando java.util.Scanner
    Parseando arquivo .csv

--- java-io/src/br/com/alura/java/io/teste/TesteLerArquivoCSV.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLerArquivoCSV.java
@@ -2,6 +2,7 @@ package br.com.alura.java.io.teste;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLerArquivoCSV {
@@ -12,6 +13,21 @@ public class TesteLerArquivoCSV {
 		while (scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
 			System.out.println(linha);
+
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
+
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useDelimiter(",");
+			linhaScanner.useLocale(Locale.US);
+
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numeroConta = linhaScanner.nextInt();
+			String nomeTitular = linhaScanner.next();
+			double saldoConta = linhaScanner.nextDouble();
+
+			linhaScanner.close();
 		}
 
 		scanner.close();
