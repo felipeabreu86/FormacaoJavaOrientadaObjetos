b69f8ea6427834d59d053b024d749c630493961c
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-05-03 07:59:03 -0300

    Usando java.util.Scanner
    Leitura de arquivo csv com Scanner

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLerArquivoCSV.java
@@ -0,0 +1,19 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TesteLerArquivoCSV {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(new File("contas.csv"));
+
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+			System.out.println(linha);
+		}
+
+		scanner.close();
+	}
+}
